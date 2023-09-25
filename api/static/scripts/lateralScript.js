3ba6033b8f09e59af432e8969e51af352521d629
d-benvenuti <d.benvenuti@diag.uniroma1.it>
2023-09-25 19:00:55 +0200

    Fixed a bug that was deleting one edge during conversion from DFG to array

--- api/static/scripts/lateralScript.js
+++ api/static/scripts/lateralScript.js
@@ -161,18 +161,19 @@ function sendDslRequest(posizione){
 
 }
 
+//function to order the steps before exporting the DSL
+function orderDslSteps(){
+	//iterate on the dslsteps 
+}
 
 //function used to compute the DSL
 function createDsl(){
-
-	var predecessors = [];
 	var parameters = [];
 	var count = [];
 	var pipelineName = document.getElementById('mapTitle').innerHTML.replace('.xes', '');
 	//print pipeline title + fixed line about communicationMedium
 	var dsl = 'Pipeline ' + pipelineName +' {\n\tcommunicationMedium: medium WEB_SERVICE\n\tsteps:\n' ;
 	//iterate on the steps
-	console.log(dslSteps)
 	for (var i=0; i<dslSteps.length; i++){
 		// if the step is start or end, just skip
 		if ( dslSteps[i][0][0].replaceAll(' ', '_') == 'start' || dslSteps[i][0][0].replaceAll(' ', '_') == 'end')
@@ -180,30 +181,11 @@ function createDsl(){
 		// first iteration no \n
 		if (i != 0)
 			dsl = dsl + '\n\n';
-		predecessors[i] = '';
 		count[i] = 0;
 		// get parameters of the current step in a string
 		parameters[i] = "\t\t\t\tFrequency: '" + dslSteps[i][0][1] + "',\n\t\t\t\tDuration: '" + dslSteps[i][0][2] +"'";
-		/* -----------------------outdated part about predecessors --------------------------------------
-		// iterate on the predecessors of the current step and save them in a string
-		for (var j=1; j<dslSteps[i].length; j++){
-			count[i]++;
-			predecessors[i] = predecessors[i] + '\t\t\t\t' + dslSteps[i][j][0].replace(' ', '_');
-			if (j<dslSteps[i].length-1)
-				predecessors[i] = predecessors[i] +',\n';	
-		}
-		// --------------------------------------------------------------------------------------------*/
 		// print the whole string for each step
 		dsl = dsl + '\t\t- data-processing step ' + dslSteps[i][0][0].replaceAll(' ', '_');
-		/* -----------------------outdated part about predecessors --------------------------------------
-		if (predecessors[i] != ''){
-			dsl = dsl + '\n\t\t\tPrevious:';
-			if (count[i]>1)
-				dsl = dsl + '[';
-			dsl = dsl + '\n' + predecessors[i];
-			if (count[i]>1)
-				dsl = dsl + '\n\t\t\t]';
-		}
 		// --------------------------------------------------------------------------------------------*/
 		dsl = dsl + "\n\t\t\timplementation: container-implementation image: ''\n\t\t\tenvironmentParameters: {\n" + parameters[i] + '\n\t\t\t}\n\t\t\tresourceProvider: Accesspoint\n\t\t\texecutionRequirement:\n\t\t\t\thardRequirements:\n';
 	}
